8bdadda0775d36898c7f0242c1338daffa61b3b6
sjmjys954646 <sjmjys954646@gmail.com>
2023-05-01 20:29:45 +0900

    fix : make in individual file

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,3 @@
-public class StringArrayConvert
-{
-    public static int[] func(String str)
-    {
-        int ret[] = new int[4];
-
-        for(int i = 0;i < str.length();i++)
-        {
-            ret[i] = str.charAt(i) - '0';
-            System.out.println( ret[i]);
-        }
-        return ret;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/StringArrayConvert.java
@@ -0,0 +1,14 @@
+public class StringArrayConvert
+{
+    public static int[] func(String str)
+    {
+        int ret[] = new int[4];
+
+        for(int i = 0;i < str.length();i++)
+        {
+            ret[i] = str.charAt(i) - '0';
+            System.out.println( ret[i]);
+        }
+        return ret;
+    }
+}
